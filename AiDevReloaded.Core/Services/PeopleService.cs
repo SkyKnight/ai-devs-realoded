105278ce996b86a7b4fdfcb7a6bfa149f19d2702
skyknight <the.skyknight@gmail.com>
2024-03-26 16:45:50 +0100

    adding implementation for inprompt task

--- AiDevReloaded.Core/Services/PeopleService.cs
+++ AiDevReloaded.Core/Services/PeopleService.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.Runtime.CompilerServices;
 
 namespace AiDevReloaded.Core.Services;
 
@@ -7,19 +9,43 @@ public sealed class PeopleService
     private readonly PeopleKnowledgeService _peopleKnowledgeService;
     private readonly string _apiKey;
 
+    private readonly Lazy<Kernel> _kernel;
+
+    private Kernel Kernel => _kernel.Value;
+
     public PeopleService(PeopleKnowledgeService peopleKnowledgeService, string apiKey)
     {
         _peopleKnowledgeService = peopleKnowledgeService;
         _apiKey = apiKey;
+        _kernel = new(() =>
+        {
+            var builder = Kernel.CreateBuilder();
+            builder.AddOpenAIChatCompletion("gpt-3.5-turbo", _apiKey);
+            return builder.Build();
+        });
     }
 
     public async Task<string> GetPersonName(string question)
     {
-        return await Task.FromResult("John Doe");
+        var prompt = $@"Z przekazanego tekstu wyodrębnij i wypisz tylko imię osoby. Nie pisz niczego ponad to.
+###
+{question}";
+
+        var storyWriter = Kernel.CreateFunctionFromPrompt(prompt, executionSettings: new OpenAIPromptExecutionSettings() { MaxTokens = 1024 });
+        var result = await Kernel.InvokeAsync(storyWriter);
+        return result.GetValue<string>();
     }
 
     public async Task<string> Ask(string question, string personName)
     {
-        return await Task.FromResult("John Doe is a person.");
+        var person = _peopleKnowledgeService.GetPerson(personName);
+
+        var prompt = $@"Korzystając z przekazanego kontekstu odpowiedz na pytanie związane z tą osobą. Na wszystkie inne odpowiadaj pomidor.
+###
+{string.Join(Environment.NewLine, person)}";
+
+        var storyWriter = Kernel.CreateFunctionFromPrompt(prompt, executionSettings: new OpenAIPromptExecutionSettings() { MaxTokens = 1024 });
+        var result = await Kernel.InvokeAsync(storyWriter);
+        return result.GetValue<string>();
     }
 }
