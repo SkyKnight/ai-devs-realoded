76c99c98bfa5709d934f426f8029a14394fab3f6
skyknight <the.skyknight@gmail.com>
2024-03-31 19:37:34 +0200

    adding whisper task

---
+++ AiDevReloaded.Core/OpenAI/Transcription/TranscriptionService.cs
@@ -0,0 +1,26 @@
+﻿using Flurl.Http;
+
+namespace AiDevReloaded.Core.OpenAI.Transcription;
+
+public sealed class TranscriptionService
+{
+    private const string API_URL = "https://api.openai.com/v1/audio/transcriptions";
+
+    private readonly string _apiKey;
+
+    public TranscriptionService(string apiKey)
+    {
+        _apiKey = apiKey;
+    }
+
+    public async Task<string> GetText(Stream stream, CancellationToken cancellationToken = default)
+    {
+        var response = await API_URL.WithOAuthBearerToken(_apiKey)
+            .PostMultipartAsync(mp => mp.AddFile("file", stream, "speech.mp3")
+                .AddString("model", "whisper-1")
+                .AddString("response_format", "text"), cancellationToken: cancellationToken)
+            .ReceiveString();
+
+        return response;
+    }
+}
---
+++ AiDevReloaded.Tests/C02L04_whisper/Test.cs
@@ -0,0 +1,22 @@
+﻿using AiDevReloaded.Core.AiTasks;
+using AiDevReloaded.Core.OpenAI.Embedding;
+using AiDevReloaded.Core.OpenAI.Transcription;
+using Flurl.Http;
+
+namespace AiDevReloaded.Tests.C02L04_whisper;
+
+public sealed class Test
+{
+    [Fact]
+    public async Task Should_Pass_Whisper_Task()
+    {
+        var taskSession = new TaskSession<DefaultResponse, string>(Secrets.AiDevTaskApiKey, "whisper");
+        var taskContent = await taskSession.GetTaskContent();
+        var transcriptionService = new TranscriptionService(Secrets.OpenAiApiKey);
+        var voiceMessage = await "https://tasks.aidevs.pl/data/mateusz.mp3".GetStreamAsync();
+        var answer = await transcriptionService.GetText(voiceMessage);
+
+
+        await taskSession.SendAnswer(answer);
+    }
+}
