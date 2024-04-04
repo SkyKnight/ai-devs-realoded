477b82d57b85d52ce843a659d69a8d410cffdfef
skyknight <the.skyknight@gmail.com>
2024-04-04 18:52:41 +0200

    adding rodo task stub

---
+++ AiDevReloaded.Tests/C03L01_rodo/Test.cs
@@ -0,0 +1,17 @@
+﻿using AiDevReloaded.Core.AiTasks;
+using Newtonsoft.Json.Linq;
+
+namespace AiDevReloaded.Tests.C03L01_rodo;
+
+public sealed class Test
+{
+    [Fact]
+    public async Task Should_Pass_Rodo_Task()
+    {
+        var taskSession = new TaskSession<DefaultResponse, JObject>(Secrets.AiDevTaskApiKey, "rodo");
+        var taskContent = await taskSession.GetTaskContent();
+        var function = JObject.Parse(await File.ReadAllTextAsync("C02L05_functions/functions.json"));
+
+        await taskSession.SendAnswer(function);
+    }
+}
